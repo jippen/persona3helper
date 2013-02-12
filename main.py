feb825c46ae305d916459bfc7b9a2dae72e8c09b
Ryan Gooler <rgooler@desk.com>
2013-02-11 22:43:00 -0800

    Good till 5/4

--- main.py
+++ main.py
@@ -15,20 +15,28 @@ class Day():
 	fileReader = csv.reader(file)
 	tmp = []
 	for line in fileReader:
-            if line[1] == day:
+            if line[0] == day:
                 tmp = line
                 break
 
-        self.current = tmp[1]
-	self.prev = tmp[2]
+        self.current = tmp[0]
+        self.dow = tmp[1]
+        self.prev = tmp[2]
         self.next = tmp[3]
 
         self.quiz = tmp[4]
         self.exam = tmp[5:9]
         self.exam = filter(None, self.exam)
-        self.where = tmp[10]
-        self.answers = tmp[11:-1]
+        self.arcana = tmp[10]
+        self.after_school = tmp[11]
+        self.answers = tmp[12:15]
         self.answers = filter(None, self.answers)
+        self.quest = tmp[16]
+        self.courage = tmp[17]
+        self.charm = tmp[18]
+        self.academic = tmp[19]
+        self.tartarus = tmp[20]
+        self.late_night = tmp[21]
 
 class MainHandler(webapp2.RequestHandler):
     def get(self, *ar, **kw):
@@ -37,11 +45,20 @@ class MainHandler(webapp2.RequestHandler):
         template_values = {
             'prev': today.prev,
             'today': today.current,
+            'dow': today.dow,
             'next': today.next,
             'quiz': today.quiz,
-            'where': today.where,
+            'arcana': today.arcana,
+            'after_school': today.after_school,
             'exam': today.exam,
-            'answers': today.answers
+            'arcana': today.arcana,
+            'answers': today.answers,
+            'quest': today.quest,
+            'courage': today.courage,
+            'charm': today.charm,
+            'academic': today.academic,
+            'tartarus': today.tartarus,
+            'late_night': today.late_night
         }
         
         template = jinja_environment.get_template('templates/date.html')
