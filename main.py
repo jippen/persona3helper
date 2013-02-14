a2adb05dbf406a9411a72d0e16867e6ab1148757
Ryan Gooler <cheetahmorph@gmail.com>
2013-02-14 02:21:34 -0800

    Use a yaml file instead

--- main.py
+++ main.py
@@ -3,69 +3,36 @@
 import webapp2
 import jinja2
 import os
-import csv
+import yaml
+from datetime import date
 from google.appengine.ext import db
+
 
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
-class Day():
-    def __init__(self, day):
-        file = open(os.path.join(os.path.dirname(__file__), 'data.csv'))
-	fileReader = csv.reader(file)
-	tmp = []
-	for line in fileReader:
-            if line[0] == day:
-                tmp = line
-                break
-
-        self.current = tmp[0]
-        self.dow = tmp[1]
-        self.prev = tmp[2]
-        self.next = tmp[3]
-
-        self.quiz = tmp[4]
-        self.exam = tmp[5:9]
-        self.exam = filter(None, self.exam)
-        self.arcana = tmp[10]
-        self.after_school = tmp[11]
-        self.answers = tmp[12:15]
-        self.answers = filter(None, self.answers)
-        self.quest = tmp[16]
-        self.courage = tmp[17]
-        self.charm = tmp[18]
-        self.academic = tmp[19]
-        self.tartarus = tmp[20]
-        self.late_night = tmp[21]
-
 class MainHandler(webapp2.RequestHandler):
     def get(self, *ar, **kw):
-        today = Day(ar[0])
-
-        template_values = {
-            'prev': today.prev,
-            'today': today.current,
-            'dow': today.dow,
-            'next': today.next,
-            'quiz': today.quiz,
-            'arcana': today.arcana,
-            'after_school': today.after_school,
-            'exam': today.exam,
-            'arcana': today.arcana,
-            'answers': today.answers,
-            'quest': today.quest,
-            'courage': today.courage,
-            'charm': today.charm,
-            'academic': today.academic,
-            'tartarus': today.tartarus,
-            'late_night': today.late_night
-        }
+        fh = open('persona3fes.yaml', 'r')
+        guide = yaml.load(fh)
+        today = guide[ar[0]]
+        today['today'] = ar[0]
+        dow  = '???'
         
+        month, day = (int(x) for x in ar[0].split('/'))
+        if month == 1:
+            d = date(2010, month, day)
+            dow = d.strftime("%A")
+        elif month != 99:
+            d = date(2009, month, day)
+            dow = d.strftime("%A")
+
+        today['dow'] = dow
+        today['courage'] = guide['courage']
+        today['charm'] = guide['charm']
+        today['academic'] = guide['academic']
+
         template = jinja_environment.get_template('templates/date.html')
-        self.response.out.write(template.render(template_values))
+        self.response.out.write(template.render(today))
 
-
-app = webapp2.WSGIApplication([
-  ('/', MainHandler),
-  (r'/([0-9]+/[0-9]+)', MainHandler),
-], debug=True)
+app = webapp2.WSGIApplication([(r'/([0-9]+/[0-9]+)', MainHandler)], debug=True)
